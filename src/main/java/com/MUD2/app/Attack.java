c0181aa5e4c31366d2118f4c67b00f792aafdd4d
724jth <61999220+724jth@users.noreply.github.com>
2022-03-04 22:02:57 -0500

    Attack and disarm commands finished

--- src/main/java/com/MUD2/app/Attack.java
+++ src/main/java/com/MUD2/app/Attack.java
@@ -22,7 +22,7 @@ public class Attack implements Command{
      */
     @Override
     public void execute() {
-
+        player.attack(target);
     }
     
 }
--- src/main/java/com/MUD2/app/Disarm.java
+++ src/main/java/com/MUD2/app/Disarm.java
@@ -1,30 +1,40 @@
 package com.MUD2.app;
-
+import java.util.Random;
 
 public class Disarm implements Command {
 /**
   * Disarm is a command responsible for removing a trap tile from a room when the 
   * player decides to take that action.
-  * 
+  *
   * @author Jahmir Hinds
   */
     private PlayerCharacter player;
-    private Tile tile;
+    private Room room;
+    int x;
+    int y;
     /**
      * Constructor for Disarm class
      * @param player The player parameter is a player character.
-     * @param tile The tile is the suspected trap tile that the player will attempt to disarm.
+     * @param room The room is the suspected the room that has a tile that the player will attempt to disarm.
+     * @param x the x coordinate of the tile attempting to be disarmed
+     * @param y the y coodrinate of the tile attempting to be disarmed
      */
-    public Disarm (PlayerCharacter player, Tile tile) {
+    public Disarm (PlayerCharacter player, Room room, int x, int y) {
         this.player = player;
-        this.tile = tile;
+        this.room = room;
     }
     /**
      * When this method is called it will have a 50 percent chance of removing a trap.
      */
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
+        Random random = new Random();
+        boolean outcome = random.nextBoolean();
+        if (outcome == true) {
+            Room.converttoempty(x,y);
+        } else {
+            Room.getTile(x,y).attack(player);
+        }
         
     }
     
