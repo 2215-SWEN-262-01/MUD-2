fc2e0a7b70c05805e439451c7d2aa5bf1e9e8590
724jth <61999220+724jth@users.noreply.github.com>
2022-03-14 17:25:30 -0400

    Player Character and NPC Fixed

--- src/main/java/com/MUD2/app/PlayerCharacter.java
+++ src/main/java/com/MUD2/app/PlayerCharacter.java
@@ -1,6 +1,6 @@
 package com.MUD2.app;
 
-public class PlayerCharacter extends Character{
+public class PlayerCharacter extends GameCharacter{
 	public static final int MAX_HEALTH = 100;
 	public static final int DEFAULT_ATTACK = 10;
 	public static final int DEFAULT_DEFENSE = 0;
