301cc33454e3609bed7654462a9c42b0c076c196
724jth <61999220+724jth@users.noreply.github.com>
2022-03-14 17:39:56 -0400

    items commands in progress

--- src/main/java/com/MUD2/app/DropItem.java
+++ src/main/java/com/MUD2/app/DropItem.java
@@ -7,31 +7,31 @@ public class DropItem implements Command {
  * @author Jahmir Hinds
  */
     private PlayerCharacter player;
-    private Item item;
+    private String itemName;
     /**
      * Constructor for the DropItem class
      * @param player The player parameter is the character of the player attempting to drop an item.
-     * @param item is the item the player is attempting to drop.
+     * @param itemName is the name of the item the player is attempting to drop.
      */
-    public DropItem (PlayerCharacter player,Item item) {
+    public DropItem (PlayerCharacter player,String itemName) {
         this.player = player;
-        this.item = item;
+        this.itemName = itemName;
     }
     /**
      * When this method is used the selected item will be removed from the player's inventory.
      */
     @Override
     public void execute() {
-        Inventory temp = player.Inventory;
+        Inventory temp = player.getInventory();
+        temp.getSpace();
         int i = 0;
         int local;
         while ( i <=5 ) {
-             int bagCount = 0;
-             if (temp.bags[bagCount].contains(item)) {
-                 local = bagCount;
-             }
-             i++;
-             player.Inventory.bags[bagCount].removeItem(item);
+            if (temp.getSpace() >= 1) {
+
+            } else {
+                System.out.println("Your bag is full!");
+            }
         }
     }
     
