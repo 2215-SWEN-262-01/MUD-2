14ddf6fec81aedc2ce3f21e582b07151b1aa07dd
Jack Ganger-Spivak <jmg9369@rit.edu>
2022-03-16 13:38:43 -0400

    Merged in WIP save system

--- src/main/java/com/MUD2/app/Main.java
+++
@@ -1,8 +0,0 @@
-package com.MUD2.app;
-
-public class Main {
-	public static void main(String[] args) {
-		PlayerCharacter me = new PlayerCharacter("NuclearMario", "cool guy");
-		System.out.println(me.getInventory().getSpace());
-	}
-}
