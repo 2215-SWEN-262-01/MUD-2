ef76ad4774b388cbff696a06ffb727477e1bf8ef
Jack Ganger-Spivak <jack.gs.code@gmail.com>
2022-04-22 17:07:10 -0400

    Figured out a workaround to get the world to update, it works well enough for now

--- src/main/java/com/MUD2/app/SwingUserInput.java
+++ src/main/java/com/MUD2/app/SwingUserInput.java
@@ -2,17 +2,35 @@ package com.MUD2.app;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SwingUserInput extends JFrame implements Runnable {
     private Map map;
-    private SwingWorldDisplay world;
+    private final SwingWorldDisplay world;
 
     public SwingUserInput(Map map) {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(640, 480);
         this.map = map;
         this.world = new SwingWorldDisplay(map);
-        this.add(world);
+        world.validate();
+        this.getContentPane().add(world);
+        this.validate();
+        
+        ActionListener timerAction = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                world.repaint();
+                //This is a pretty dumb way of doing things but it does work for now
+                //Ideally repaint would only be called when each turn is finished
+                //But for some reason that doesn't work so timer it is
+            }
+        };
+        Timer timer = new Timer(16, timerAction); //update at 60fps :D
+        timer.setRepeats(true);
+        timer.start();
     }
 
     @Override
@@ -22,7 +40,6 @@ public class SwingUserInput extends JFrame implements Runnable {
     }
 
     public void displayRoom() {
-        revalidate();
-        repaint();
+        world.repaint();
     }
 }
