a37f3c11f15013a2b0f1975f337bb75f9195f396
724jth <61999220+724jth@users.noreply.github.com>
2022-04-25 08:51:20 -0400

    shrine tile completed

---
+++ src/main/java/com/MUD2/app/ShrineTile.java
@@ -0,0 +1,7 @@
+package com.MUD2.app;
+
+public class ShrineTile extends Tile{
+    public ShrineTile(int horizantalLocation, int verticalLocation, GameCharacter character) {
+        super(horizantalLocation, verticalLocation, character);
+    }
+}
